6e151d62b095d85802f88c302eb3419dacd8882d
borabiricik <bora.biricik>
2022-09-18 15:20:07 +0300

    Chevron Icon fix

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 import { PhoneInput } from 'react-international-phone-number-input'
 import 'react-international-phone-number-input/dist/index.css'
@@ -9,10 +8,18 @@ function App() {
   return (
     <div>
       <PhoneInput
+        containerProps={{}}
         defaultCountry='KW'
+        dropdownButtonProps={{
+          // flagWidth: 30,
+          // flagHeight: 30
+          minWidth: 25
+        }}
+        disableCountrySelect={false}
         flagProps={{
           rounded: true
         }}
+        inputProps={{}}
         onInputChange={(values) => {
           console.log(values)
         }}
--- src/assets/ChevronIcon.tsx
+++ src/assets/ChevronIcon.tsx
@@ -5,29 +5,28 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   dropdownOpen: boolean
 }
 
-const IconContainer = styled.div<Props>`
+const IconContainer = styled.svg<Props>`
   transition: all 0.3s;
   transform: ${(props) =>
-    props.dropdownOpen ? 'rotateY(0deg)' : 'rotateY(180deg)'};
+    props.dropdownOpen ? 'rotate(0deg)' : 'rotate(180deg)'};
   width: 12px;
   height: 12px;
 `
 
 const ChevronIcon = (props: Props) => {
   return (
-    <IconContainer dropdownOpen={props.dropdownOpen}>
-      <svg
-        xmlns='http://www.w3.org/2000/svg'
-        width='12'
-        height='12'
-        viewBox='0 0 12.453 7.12'
-      >
-        <path
-          id='ic_chevron_up'
-          d='M6.229,2.146l4.709,4.712a.886.886,0,0,0,1.257,0,.9.9,0,0,0,0-1.261L6.859.259A.888.888,0,0,0,5.632.233L.26,5.594A.89.89,0,0,0,1.516,6.855Z'
-          fill='#1b1d21'
-        />
-      </svg>
+    <IconContainer
+      dropdownOpen={props.dropdownOpen}
+      xmlns='http://www.w3.org/2000/svg'
+      width='12'
+      height='12'
+      viewBox='0 0 12.453 7.12'
+    >
+      <path
+        id='ic_chevron_up'
+        d='M6.229,2.146l4.709,4.712a.886.886,0,0,0,1.257,0,.9.9,0,0,0,0-1.261L6.859.259A.888.888,0,0,0,5.632.233L.26,5.594A.89.89,0,0,0,1.516,6.855Z'
+        fill='#1b1d21'
+      />
     </IconContainer>
   )
 }
